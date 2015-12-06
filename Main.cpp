f864cb1021cadffffa142b884d4fe16c1a8fb8d4
NSuri1 <nalinsuri@gmail.com>
2015-12-06 15:33:15 -0500

    STill nothing

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,7 @@ void nalin();
 
 int main()
 {
-    
+    nalin();
     return 0;
 }
 
@@ -27,22 +27,27 @@ cout<<"Test \n";
 
 void nalin()
 {
-	const int SIZE=2500;
-	long arr[SIZE]={1, 1};
-  vector<long> fibnums(2, 1);
+//	const int SIZE=2500;
+//	long arr[SIZE]={1, 1};
+  	vector<int> fibnums;
     int end=0;
-    int count=2;
+    int first=0;
+    int second=1;
     
     cout<< "Enter a number to stop the fibonacci sequence before\n";
     cin>> end;
-    cout<<arr[0]<< ", "<< arr[1]<< ", ";
     
-    for(int count=2; arr[count]<=end; count++)
-    {
-    	arr[count]= arr[count-2]+arr[count-1];
-    	cout<< arr[count]<<", ";
-    	
-	}
+    fibnums[0]=second;
+    fibnums[1]=first+second;
+    
+    for (int i=2; )
+    
+    
+//    cout<<arr[0]<< ", "<< arr[1]<< ", ";
+//    for(int i=2; arr[i]<=end; count++)
+//    {
+//    	cout<< arr[i]<<", ";	
+//	}
     
 //    cout<<fibnums[0]<< ", "<< fibnums[1]<< ", ";
 //    
