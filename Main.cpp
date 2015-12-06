9b999eefd91f2276d4d0602b1371850fba557ad9
NSuri1 <nalinsuri@gmail.com>
2015-12-06 15:02:48 -0500

    fg

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,7 @@ void nalin();
 
 int main()
 {
-    
+
     
     return 0;
 }
@@ -28,25 +28,31 @@ cout<<"Test \n";
 
 void nalin()
 {
-  vector<int> fibnums(2);
+  vector<long> fibnums(2, 1);
     int end=0;
-    
-   
-    fibnums[0]=0;
-    fibnums[1]=1;
+    int count=2;
     
     cout<< "Enter a number to stop the fibonacci sequence before\n";
     cin>> end;
     
-//    while (fibnums[count]<= end)
-//    {
-//        cout<< fibnums[count]<<endl;
-//        count++;
-//    }
+    cout<<fibnums[0]<< ", "<< fibnums[1]<< ", ";
     
-    for(int count=2; fibnums[count]<=end; count++)
+    while (fibnums[count]<= end)
     {
     	fibnums[count]= fibnums[count-2]+fibnums[count-1];
-    	cout<< fibnums[count]<<endl;
-	}
+      cout<< fibnums[count]<< ", ";
+        count++;
+//        for (int val : fibnums)
+//		{
+//			cout << val << endl; 
+//		}
+    }
+    
+	    
+//	for(int count=2; fibnums[count]<=end; count++)
+//    {
+//    	fibnums[count]= fibnums[count-2]+fibnums[count-1];
+//    	cout<< fibnums[count]<<endl;
+//	}
+	
 }
