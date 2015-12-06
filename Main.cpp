4faf59112c5eaefeda4db538d9357a87d6e78a68
NSuri1 <nalinsuri@gmail.com>
2015-12-06 15:24:36 -0500

    Trying arrays

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,6 @@ void nalin();
 
 int main()
 {
-
     
     return 0;
 }
@@ -28,27 +27,37 @@ cout<<"Test \n";
 
 void nalin()
 {
+	const int SIZE=2500;
+	long arr[SIZE]={1, 1};
   vector<long> fibnums(2, 1);
     int end=0;
     int count=2;
     
     cout<< "Enter a number to stop the fibonacci sequence before\n";
     cin>> end;
+    cout<<arr[0]<< ", "<< arr[1]<< ", ";
     
-    cout<<fibnums[0]<< ", "<< fibnums[1]<< ", ";
-    
-    while (fibnums[count]<= end)
+    for(int count=2; arr[count]<=end; count++)
     {
-    	fibnums[count]= fibnums[count-2]+fibnums[count-1];
-      cout<< fibnums[count]<< ", ";
-        count++;
+    	arr[count]= arr[count-2]+arr[count-1];
+    	cout<< arr[count]<<", ";
+    	
+	}
+    
+//    cout<<fibnums[0]<< ", "<< fibnums[1]<< ", ";
+//    
+//    while (fibnums[count]<= end)
+//    {
+//    	fibnums[count]= fibnums[count-2]+fibnums[count-1];
+//      cout<< fibnums[count]<< ", ";
+//        count++;
 //        for (int val : fibnums)
 //		{
 //			cout << val << endl; 
 //		}
-    }
-    
-	    
+//    }
+//    
+//	    
 //	for(int count=2; fibnums[count]<=end; count++)
 //    {
 //    	fibnums[count]= fibnums[count-2]+fibnums[count-1];
