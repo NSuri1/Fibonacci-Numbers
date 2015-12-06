ea8c246a552f385a67f26c675c1aa38403c6451d
NSuri1 <nalinsuri@gmail.com>
2015-12-06 14:35:02 -0500

    Used for loop instead still not working

--- Main.cpp
+++ Main.cpp
@@ -5,23 +5,25 @@ int main()
 {
     vector<int> fibnums(2);
     int end=0;
-    int count=2;
     
-    fibnums[count]=fibnums[count-2]+fibnums[count-1];
+   
     fibnums[0]=0;
     fibnums[1]=1;
     
     cout<< "Enter a number to stop the fibonacci sequence before\n";
     cin>> end;
     
-    while (fibnums[count]<= end)
+//    while (fibnums[count]<= end)
+//    {
+//        cout<< fibnums[count]<<endl;
+//        count++;
+//    }
+    
+    for(int count=2; fibnums[count]<=end; count++)
     {
-//    	if(count=0)
-//    	{
-//    		break;
-//		}
-        cout<< fibnums[count]<<endl;
-        count++;
-    }
+    	fibnums[count]= fibnums[count-2]+fibnums[count-1];
+    	cout<< fibnums[count]<<endl;
+	}
+    
     return 0;
 }
